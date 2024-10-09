4877be5b3129b3ea3e886685850146e3191bbd38
TaranDahl <sd_zealot@163.com>
2024-10-09 13:10:07 +0800

    Revert "Merge branch 'ScriptRelated' into MJTest"
    This reverts commit e60af2bf2bc291262877b88b637067aeb0cf0626, reversing
    changes made to 13685ce82e9ed2dca62e528556ff508dfb8cff95.

--- src/Ext/Script/Hooks.cpp
+++ src/Ext/Script/Hooks.cpp
@@ -133,15 +133,7 @@ DEFINE_HOOK(0x6ECCF3, TeamClass_UpdateScriptAction16_UsePhobosTargeting, 0x7)
 		pTeamData->TeamLeader = pLeaderUnit;
 	}
 
-	if (!pLeaderUnit)
-	{
-		return FuncRet;
-	}
-
-	auto const pLeaderUnitType = pLeaderUnit->GetTechnoType();
 	CellClass* pCell = nullptr;
-
-	// Args process
 	scriptArg = -scriptArg;
 	int TargetType = -1;
 	int targetMask = -1;
@@ -166,13 +158,7 @@ DEFINE_HOOK(0x6ECCF3, TeamClass_UpdateScriptAction16_UsePhobosTargeting, 0x7)
 	}
 
 	auto selectedTarget = ScriptExt::FindBestObject(pLeaderUnit, targetMask, calcThreatMode, pickAllies, TargetType, -1, needAttackableByLeader);
-
-	if (selectedTarget)
-	{
-		bool isBridge = selectedTarget->IsOnBridge();
-		auto cell = MapClass::Instance->NearByLocation(selectedTarget->GetMapCoords(), pLeaderUnitType->SpeedType, -1, pLeaderUnitType->MovementZone, isBridge, 1, 1, false, false, false, isBridge, CellStruct::Empty, false, false);
-		pCell = MapClass::Instance->TryGetCellAt(cell);
-	}
+	pCell = selectedTarget ? selectedTarget->GetCell() : nullptr;
 
 	if (!pCell)
 	{
